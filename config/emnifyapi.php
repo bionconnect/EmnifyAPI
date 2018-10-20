957f3740d7c7e26a8ff10f023367fcfc3fa64660
Ivan berthillood <Ivan berthillood@Ivanberthillood>
2018-10-20 20:41:38 -0300

    Agregue las funciones que faltan

--- config/emnifyapi.php
+++ config/emnifyapi.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    //
+  'api_token'=>'',
+   'api_endpoint'=> "https://cdn.emnify.net/api/v1"
 ];
--- src/EmnifyAPI.php
+++ src/EmnifyAPI.php
@@ -2,52 +2,241 @@
 
 namespace BionConnection\EmnifyAPI;
 
-class EmnifyAPI
-{
-    const TIMEOUT = 10;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Facades\Cache;
+use \GuzzleHttp\Psr7\Uri;
 
-    private $api_token = "eyJhbGciOiJIUzUxMiJ9.eyJlc2MuYXBwc2VjcmV0IjoiYzNkYWIzMTgtYzQ4Yy00MTY1LWI5OTgtNjA1MDZkNmEwZTkwIiwic3ViIjoiaXZhbi5zeXNAZ3J1cG9iaW9uLmNvbSIsImF1ZCI6IlwvYXBpXC92MVwvYXBwbGljYXRpb25fdG9rZW4iLCJlc2MuYXBwIjoxMzkwLCJhcGlfa2V5IjpudWxsLCJlc2MudXNlciI6MjAwMTc1LCJlc2Mub3JnIjo2MDEwLCJlc2Mub3JnTmFtZSI6IkdSVVBPIEJJT04iLCJpc3MiOiJzcGMtZnJvbnRlbmQwMDFAc3BjLWZyb250ZW5kIiwiaWF0IjoxNTQwMDQ5MTczfQ.Gnr_mioFAuEY1HWZdeIlkS7jSBuL1dpximPE9ZqUy6DhHTHYJ4p01kkFlE0USjNkAiXd1ltapAJlkHLm6rvMJA";
-    private $api_endpoint = "https://cdn.emnify.net/api/v1";
+class EmnifyAPI {
+
+    const TIMEOUT = 10;
+    const STATUS_SIM_NO_USADA = "0";
+    const STATUS_EP_ENABLED = 0;
+    const STATUS_EP_DISABLED = 1;
+    const STATUS_EP_DELETED = 2;
+
+    private $api_token = "";
+    private $api_endpoint = "";
+    private $auth_token;
+    private $request_successful = false;
+    private $last_error = '';
+    private $last_response = array();
+    private $last_request = array();
+    private $period_valid_token = 5;
+    private $arrSucess = ['response' => 'success'];
 
     public function __construct() {
 
-       /* $this->username  = config('emnifyapi.uri_key_pem');
-        $this->uri_ca_pem = config('movistarm2m.uri_ca_pem');
-        $this->response = null;
-        $this->api_endpoint = config('movistarm2m.url');*/
-        
-        $this->client = new Client(['http_errors' => true]);
-        
+        $this->auth_token = Cache::get('auth_token');
+        $this->client = new Client(['http_errors' => false,
+            'headers' => ['Content-Type' => 'application/json',]
+        ]);
+
+        $this->api_token = config('emnifyapi.api_token');
+        $this->api_endpoint =config('emnifyapi.api_endpoint');
+        $this->autenticate();
     }
-    private function autenticate(){
-        $arrToken = array("application_token"=> $this->api_token);
-        
-        print_r($this->makeRequest("GET","authenticate"));
-        
+
+    public function GetConnectionStatus($iccid) {
+
+        $endPoint = $this->getEndPoint($iccid)[0];
+        return $this->makeRequest("get", "endpoint/" . $endPoint->id . "/connectivity");
     }
-     private function makeRequest($http_verb, $method, $args = array(), $timeout = self::TIMEOUT) {
+
+    public function GetLocationStatus($iccid) {
+        $endPoint = $this->getEndPoint($iccid)[0];
+        return $this->makeRequest("get", "endpoint/" . $endPoint->id . "/connectivity_info");
+    }
+
+    public function changeEndPointServiceProfile($iccid, $new_id_service_profile) {
+        $endPoint = $this->getEndPoint($iccid)[0];
+        $arrUpdate = ["service_profile" => ["id" => $new_id_service_profile]];
+        $this->updateEndPoint($id_end_point, $arrUpdate);
+    }
+
+    public function getSims($iccid = null, $inactive_new = false) {
+        $query = "";
+
+        if ($inactive_new) {
+            $query = "status:" . STATUS_SIM_NO_USADA;
+        }
+
+        if (isset($iccid)) {
+            $query .= "iccid:" . $iccid;
+        }
+
+        return $this->makeRequest("get", "sim?q=" . $query);
+    }
+
+    private function updateEndPoint($id_end_point, $arrUpdate) {
+        $this->makeRequest("patch", "endpoint/" . $id_end_point, $arrUpdate);
+    }
+
+    private function relaceEndPointSim($id_end_point) {
+
+        $this->updateEndPoint($id_end_point, ["sim" => ["id" => null]]);
+    }
+
+    private function getEndPoint($iccid = null, $inactive_new = false) {
+        $query = "";
+
+        if ($inactive_new) {
+            $query = "status:" . STATUS_SIM_NO_USADA;
+        }
+
+        if (isset($iccid)) {
+            $query .= "iccid:" . $iccid;
+        }
+
+        return $this->makeRequest("get", "endpoint?q=" . $query);
+    }
+
+    private function createEndPoint($id_service_profile, $id_sim) {
+        $default_tariff_profile = 188198;
+        $arrparams = ['name' => ' EndPoint Clien ' . $id_sim,
+            'status' => ['id' => self::STATUS_EP_ENABLED],
+            'service_profile' => ['id' => $id_service_profile],
+            'tariff_profile' => ['id' => $default_tariff_profile],
+            'sim' => ['id' => $id_sim, 'activate' => true]
+        ];
+
+        $this->makeRequest("post", "endpoint", $arrparams);
+
+        return $this->request_successful;
+    }
+
+    public function activateSim($icc, $id_service_profile) {
+
+        $sim = $this->getSims($icc)[0];
+
+
+        if ($this->createEndPoint($id_service_profile, $sim->id)) {
+
+            return $this->arrSucess;
+        }
+    }
+
+    private function deleteEndPoint($iccid) {
+
+        $endpoint = $this->getEndPoint($iccid)[0];
+        $this->relaceEndPointSim($endpoint->id);
+        $this->makeRequest("delete", "endpoint/" . $endpoint->id);
+
+        if ($this->request_successful) {
+
+            return $this->arrSucess;
+        }
+    }
+
+    private function deleteSim($iccid) {
+        $sim = $this->getSims($iccid)[0];
+        $this->makeRequest("delete", "sim/" . $sim->id);
+
+
+        if ($this->request_successful) {
+
+            return $this->arrSucess;
+        }
+    }
+
+    public function terminateSim($iccid) {
+
+        $this->deleteEndPoint($iccid);
+
+        $this->deleteSim($iccid);
+    }
+
+    private function autenticate() {
+        if (!$this->auth_token) {
+            $arrToken = array("application_token" => $this->api_token);
+            $response = $this->client->post($this->getUrlApi('authenticate'), ['body' => json_encode($arrToken)]);
+            switch ($response->getStatusCode()) {
+                case 200:
+                    $login = json_decode($response->getBody());
+                    Cache::put('auth_token', $login->auth_token, $this->period_valid_token);
+                    $this->auth_token = $login->auth_token;
+                    break;
+                case 401 :
+                    $this->last_error = sprintf('%d: Token invalido', $response->getStatusCode());
+                    break;
+            }
+        }
+    }
+
+    public function suspedSim($iccid) {
+
+        $endPoint = $this->getEndPoint($iccid)[0];
+        $arrUpdate = ["status" => self::STATUS_EP_DISABLED];
+        $this->updateEndPoint($endPoint->id, $arrUpdate);
+    }
+
+    public function unSuspendSim($iccid) {
+        $endPoint = $this->getEndPoint($iccid)[0];
+        $arrUpdate = ["status" => self::STATUS_EP_ENABLED];
+        $this->updateEndPoint($endPoint->id, $arrUpdate);
+    }
+
+    private function getUrlApi($method) {
+        return $this->api_endpoint . '/' . $method;
+    }
+
+    private function makeRequest($http_verb, $method, $args = array(), $timeout = self::TIMEOUT) {
         unset($this->response);
-        $url = $this->api_endpoint . '/' . $method;
+        $this->request_successful = false;
+        $headers = ['Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $this->auth_token];
+
         switch ($http_verb) {
             case 'post':
+                $this->response = $this->client->post($this->getUrlApi($method), ['body' => json_encode($args), 'headers' => $headers]);
+
                 break;
             case 'get':
-                $this->response = $this->client->get($url, ['cert' => $this->uri_ca_pem, 'ssl_key' => $this->uri_key_pem, 'query' => $args, 'timeout' => $timeout])->getBody();
-                return $this->response;
+                $this->response = $this->client->get($this->getUrlApi($method), ['headers' => $headers]);
                 break;
             case 'delete':
+                $this->response = $this->client->delete($this->getUrlApi($method), ['headers' => $headers]);
 
                 break;
             case 'patch':
-
+                $this->response = $this->client->patch($this->getUrlApi($method), ['body' => json_encode($args), 'headers' => $headers]);
                 break;
             case 'put':
-                return $this->client->put($url, ['cert' => $this->uri_ca_pem, 'ssl_key' => $this->uri_key_pem, 'body' => $args, 'timeout' => $timeout])->getBody();
+                // return $this->client->put($url, ['cert' => $this->uri_ca_pem, 'ssl_key' => $this->uri_key_pem, 'body' => $args, 'timeout' => $timeout])->getBody();
 
+                break;
+        }
+
+        echo $this->response->getStatusCode();
+
+        switch ($this->response->getStatusCode()) {
+            case 200:
+                $this->request_successful = true;
+                return json_decode($this->response->getBody());
+                break;
+            case 401;
+                $this->revokeToken();
+                $this->makeRequest($http_verb, $method, $args = array(), $timeout = self::TIMEOUT);
+                break;
+            case 204: //borrado correctamente
+                $this->request_successful = true;
+                break;
+            case 409:
+
+            default :
+                return json_decode($this->response->getBody());
                 break;
         }
     }
 
-    
+    private function revokeToken() {
+        $this->auth_token = false;
+        $this->autenticate();
+    }
+
+    public function getLastError() {
+        return $this->last_error ?: false;
+    }
 
 }
